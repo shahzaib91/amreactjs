a1c23cb16b5ddfcb7659797595c0eed5c33b8127
Shahzaib <Shahzaib.2377@gmail.com>
2022-08-21 14:36:33 +0500

    Header template

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import {Container, Nav, Navbar} from 'react-bootstrap';
 import Logo from '../logo.svg';
+import { List, Plus } from 'react-bootstrap-icons';
 
 export default function Header() {
     return (
-        <Navbar bg="light" expand="lg">
-            <Container fluid>
+        <Navbar bg="light" expand="lg" fixed='top'>
+            <Container>
                 <Navbar.Brand to="/">
                     <img src={Logo} width="50px" alt="AwesomeMotive Blog" />
                     AwesomeMotive Blog
@@ -14,8 +15,8 @@ export default function Header() {
                 <Navbar.Toggle aria-controls="navbarScroll" />
                 <Navbar.Collapse id="navbarScroll">
                     <Nav className="me-auto my-2 my-lg-0" style={{ maxHeight: '100px' }} navbarScroll></Nav>
-                    <Link className='btn btn-primary mx-2' to="/">All Posts</Link>
-                    <Link className='btn btn-danger' to="/createPost">New Post</Link>
+                    <Link className='btn btn-primary mx-2' to="/"><List /> All Posts</Link>
+                    <Link className='btn btn-danger' to="/createPost"><Plus /> New Post</Link>
                 </Navbar.Collapse>
             </Container>
         </Navbar>
