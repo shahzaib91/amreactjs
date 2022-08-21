4153801c3d7bdd4dc41bda07aadf9a87cdf89742
Shahzaib <Shahzaib.2377@gmail.com>
2022-08-21 14:36:08 +0500

    Article item template

--- src/components/ArticleItem.jsx
+++ src/components/ArticleItem.jsx
@@ -1,14 +1,21 @@
 import React from 'react'
 import PropTypes from 'prop-types';
+import { Clock, Pencil, Trash } from 'react-bootstrap-icons';
+import { Link } from 'react-router-dom';
+
 
 export default function ArticleItem({title, content, timestamp}) {
   return (
-    <>
-        <h3>{title.toUpperCase()}</h3>
-        <small>Posted on: {timestamp}</small>
-        <p>{content.substr(1,100)+"..."}</p>
-        <hr style={{color:"#000"}} />
-    </>
+    <div className="row">
+        <div className="col-md-12">
+            <h3>{title.toUpperCase()}</h3>
+            <small><Clock /> {timestamp}</small>
+            <p className="my-2">{content.substr(0,180)+"..."}</p>
+            <Link className='btn btn-success btn-sm' to="/editPost"><small><Pencil /></small></Link>
+            <Link className='btn btn-danger  btn-sm mx-2' to="/editPost"><small><Trash /></small></Link>
+            <hr style={{color:"#000"}} />
+        </div>
+    </div>
   )
 }
 
