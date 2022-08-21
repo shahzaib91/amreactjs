57f097ad28d429a89b1f8c43051eb7ec46e6a227
Shahzaib <Shahzaib.2377@gmail.com>
2022-08-21 14:41:13 +0500

    Axios, Redux, React Redux packages are added Boiler plate code is moved Default test file is moved Home template is modified

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ArticleItem from '../components/ArticleItem'
 
-export default function Home() {
-  return (
-    <>
-        <ArticleItem title="" content="" timestamp="" />
-    </>
-  )
+export default function Home() 
+{
+    return (
+        <>
+            <ArticleItem title="What is Lorem Ipsum?" content="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum." timestamp="2022-08-20 11:15 PM" />
+        </>
+    )
 }
