4f094052c5b6ce7f597eed4cbdd0102970566679
Shahzaib <Shahzaib.2377@gmail.com>
2022-08-22 15:46:45 +0500

    App name is now being used from config file

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,14 +3,20 @@ import { Link } from 'react-router-dom'
 import {Container, Nav, Navbar} from 'react-bootstrap';
 import Logo from '../logo.svg';
 import { List, Plus } from 'react-bootstrap-icons';
+import Config from '../globals/Config';
 
 export default function Header() {
+
+    const {appName} = Config();
+
     return (
         <Navbar bg="light" expand="lg" fixed='top'>
             <Container>
-                <Navbar.Brand to="/">
-                    <img src={Logo} width="50px" alt="AwesomeMotive Blog" />
-                    AwesomeMotive Blog
+                <Navbar.Brand>
+                    <Link to="/" style={{textDecoration:"none", color:"#000"}}>
+                        <img src={Logo} width="50px" alt="AwesomeMotive Blog" />
+                        {appName}
+                    </Link>
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="navbarScroll" />
                 <Navbar.Collapse id="navbarScroll">
