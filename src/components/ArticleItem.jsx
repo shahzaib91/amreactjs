d70a70152a39846ba9bfdbadd4b431188631a626
Shahzaib <Shahzaib.2377@gmail.com>
2022-08-22 15:46:20 +0500

    Template is now fully dynamic

--- src/components/ArticleItem.jsx
+++ src/components/ArticleItem.jsx
@@ -2,26 +2,47 @@ import React from 'react'
 import PropTypes from 'prop-types';
 import { Clock, Pencil, Trash } from 'react-bootstrap-icons';
 import { Link } from 'react-router-dom';
+import Moment from 'moment';
+import HttpHandler from '../globals/HttpHandler';
+import { useDispatch } from 'react-redux';
+import { delBlogItem } from '../redux/actions/BlogActions';
 
 
-export default function ArticleItem({title, content, timestamp}) {
+export default function ArticleItem({ id, title, content, timestamp }) {
+
+  const date        = new Date(timestamp);
+  const {http}      = HttpHandler();
+  const dispatch    = useDispatch();
+
+  const onDelete = async (id) => {
+    if (id!=="" && window.confirm("Are you sure you want to delete this item?")) {
+      const response = await http.get(`deleteBlog/${id}`).catch((err)=>{
+        // console.log(err);
+      });
+      if(response.data.status===true){
+        dispatch(delBlogItem(id));
+      }
+    }
+  }
+
   return (
     <div className="row">
-        <div className="col-md-12">
-            <h3>{title.toUpperCase()}</h3>
-            <small><Clock /> {timestamp}</small>
-            <p className="my-2">{content.substr(0,180)+"..."}</p>
-            <Link className='btn btn-success btn-sm' to="/editPost"><small><Pencil /></small></Link>
-            <Link className='btn btn-danger  btn-sm mx-2' to="/editPost"><small><Trash /></small></Link>
-            <hr style={{color:"#000"}} />
-        </div>
+      <div className="col-md-12">
+        <h3><Link to={'/article/' + id}>{title.toUpperCase()}</Link></h3>
+        <small><Clock /> {Moment(date).format('MMM DD, YYYY') + " AT " + Moment(date).format('hh:mm:ss A')}</small>
+        <p className="my-2">{content.substr(0, 180) + "..."}</p>
+        <Link className='btn btn-success btn-sm' to={`/editPost/${id}`}><small><Pencil /></small></Link>
+        <Link className='btn btn-danger  btn-sm mx-2' to="#" onClick={() => (onDelete(id))}><small><Trash /></small></Link>
+        <hr style={{ color: "#000" }} />
+      </div>
     </div>
   )
 }
 
 ArticleItem.propTypes =
 {
-    title       : PropTypes.string.isRequired,
-    content     : PropTypes.any.isRequired,
-    timestamp   : PropTypes.any.isRequired,
+  id: PropTypes.number.isRequired,
+  title: PropTypes.string.isRequired,
+  content: PropTypes.any.isRequired,
+  timestamp: PropTypes.any.isRequired,
 }
