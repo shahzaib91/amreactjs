8b6e248075738f88f77435fd50a79b68d32ddf21
Shahzaib <Shahzaib.2377@gmail.com>
2022-08-22 15:45:59 +0500

    Additional routes configured

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './routes/Home';
-
+import Article from './routes/Article';
+import NotFound from './routes/NotFound';
+import CreateArticle from './routes/CreateArticle';
+import EditArticle from './routes/EditArticle';
 
 function App() {
   return (
@@ -10,7 +13,11 @@ function App() {
       <Header />
       <div className='container mb-3' style={{ marginTop: "80px" }}>
         <Routes>
-          <Route path='/' element={<Home />}></Route>
+          <Route path='/' element={<Home />} />
+          <Route path='/article/:id' element={<Article />} />
+          <Route path='/createPost' element={<CreateArticle />} />
+          <Route path='/editPost/:id' element={<EditArticle />} />
+          <Route path='*' element={<NotFound />} />
         </Routes>
       </div>
     </>
