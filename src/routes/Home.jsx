79d19cd38bb34a35a32c7fa435b8080adda75337
Shahzaib <Shahzaib.2377@gmail.com>
2022-08-22 15:48:53 +0500

    Dynamic articles fetching is configured

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,20 +1,21 @@
 import React, {useEffect} from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { setBlogList } from "../redux/actions/BlogActions";
+import { setError, resetError } from '../redux/actions/ValidationActions';
 import ArticleItem from '../components/ArticleItem';
 import HttpHandler from '../globals/HttpHandler';
 import { ExclamationCircleFill } from 'react-bootstrap-icons';
 
 export default function Home() 
 {
-    const {BlogData} = useSelector((state)=>state);
-    const {http, baseUrl} = HttpHandler();
-    const dispatch = useDispatch()
+    const {BlogList}        = useSelector((state)=>state.BlogData);
+    const {http, baseUrl}   = HttpHandler();
+    const dispatch          = useDispatch();
+    const error             = useSelector((state) => state.Validation);
 
     const fetcBlogList = async () => {
         const response = await http.get(baseUrl+"readBlogs").catch((err)=>{
-            console.log(err);
-            alert("Failed! "+err.code+" "+err.message);
+            dispatch(setError(err.code+" "+err.message));
         });
         if(response.data.records!=null)
         {
@@ -23,14 +24,16 @@ export default function Home()
     }
 
     useEffect(() => {
+        dispatch(resetError());
         fetcBlogList();
     },[]);
 
     return (
         <>
+            {error && (<div className='alert alert-danger my-2'>{error}</div>)}
             {
-                BlogData.BlogList.length>0 ? 
-                BlogData.BlogList.map( (item, i) => ( <ArticleItem key={i} title={item.title} content={item.content} timestamp={item.timestamp} /> )) 
+                BlogList.length>0 ? 
+                BlogList.map( (item, i) => ( <ArticleItem key={i} id={item.id} title={item.title} content={item.content} timestamp={item.timestamp} /> )) 
                 : (<div className='alert alert-info'><ExclamationCircleFill /> No articles to display!</div>)
             }
         </>
